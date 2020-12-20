cbe146b7cf210110541711d7c2a5ee4031dac6df
Alex Curran <alexcurran.va@gmail.com>
2020-12-20 10:55:16 -0600

    Created base of User model

--- api/models/User.js
+++ api/models/User.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, min: 1, max: 100, required: true },
+    email: { type: String, min: 1, max: 100, required: true },
+    password: { type: String, min: 6, max: 100, required: true },
+    bio_description: { type: String, max: 1000 },
+    followers: { type: Array },
+    created_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("User", UserSchema);
