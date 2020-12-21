0254fea14aa9b6e13070b0a73ddd4c6542c2b83f
Alex Curran <alexcurran.va@gmail.com>
2020-12-21 13:01:14 -0600

    Added various tables for schema

---
+++ db/db_schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE app_user(
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    bio_description VARCHAR,
+    avatar_img_url VARCHAR
+);
+
+CREATE TABLE app_user_follower(
+    follower_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE artwork(
+    artwork_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
+    title VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE artwork_like(
+    like_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE artwork_comment(
+    comment_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE artwork_favorite(
+    favorite_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
+);
