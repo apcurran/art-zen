23cc6c2d51414621f4aace9d5fe15ef9dca20f53
Alex Curran <alexcurran.va@gmail.com>
2020-12-21 16:26:49 -0600

    Updated seeds.js file, for populating db, with a func for retrieving cloudinary public image ids to use in main populateDb func

---
+++ db/seeds.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const { cloudinary } = require("../utils/cloudinary");
+const db = require("../db/index");
+
+async function getCloudinaryPublicIds() {
+    const { resources } = await cloudinary
+                                    .search
+                                    .expression("folder:art-zen-app")
+                                    .execute();
+    const publicIds = resources.map(file => file.public_id);
+    
+    return publicIds;
+}
+
+(async function populateDb() {
+    console.log(await getCloudinaryPublicIds());
+})();
