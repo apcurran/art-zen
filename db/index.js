0a14fe51aa9e523a79aa177fac370072fad37e98
Alex Curran <alexcurran.va@gmail.com>
2020-12-21 11:26:13 -0600

    Adjusted dotenv var

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DB_URI,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
