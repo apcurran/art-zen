7c996b9516d7fbfd1572fd4306592cd5bd36ce63
Alex Curran <alexcurran.va@gmail.com>
2020-12-22 10:41:24 -0600

    Finished seeds.js script to populate db

--- db/seeds.js
+++ db/seeds.js
@@ -6,6 +6,7 @@ const { cloudinary } = require("../utils/cloudinary");
 const db = require("../db/index");
 const SQL = require("sql-template-strings");
 const faker = require("faker");
+const bcrypt = require("bcrypt");
 
 async function getCloudinaryPublicIds() {
     const { resources } = await cloudinary
@@ -24,18 +25,34 @@ async function getCloudinaryPublicIds() {
         // Create app_user account
         const username = faker.internet.userName();
         const email = faker.internet.email();
-        const password = process.env.DB_SEEDS_PW;
         const bio_description = faker.lorem.sentences(3);
         const avatar_img_url = faker.image.abstract();
 
-        console.log({ username, email, password, bio_description, avatar_img_url });
+        // Hash pw
+        const saltRounds = 12;
+        const password = process.env.DB_SEEDS_PW;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
 
         // Save in db
-        // const savedUser = await db.query(SQL`
-        //     INSERT INTO app_user()
-        // `);
+        const savedUser = await db.query(SQL`
+            INSERT INTO app_user
+                (username, email, password, bio_description, avatar_img_url)
+            VALUES
+                (${username}, ${email}, ${hashedPassword}, ${bio_description}, ${avatar_img_url})
+            RETURNING *
+        `);
 
         // Create artwork row entry and link with foreign key from app_user.user_id
         const { user_id } = savedUser.rows[0];
+        const title = faker.company.catchPhrase();
+        const description = faker.lorem.sentences(2);
+        const img_url = img;
+
+        await db.query(SQL`
+            INSERT INTO artwork
+                (user_id, title, description, img_url)
+            VALUES
+                (${user_id}, ${title}, ${description}, ${img_url})
+        `);
     }    
 })();
