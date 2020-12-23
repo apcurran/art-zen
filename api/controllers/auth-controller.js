a79a7ecce83abfc0eb50feacdafc13c87d4bf450
Alex Curran <alexcurran.va@gmail.com>
2020-12-23 15:17:17 -0600

    User log in added with JWT

--- api/controllers/auth-controller.js
+++ api/controllers/auth-controller.js
@@ -47,7 +47,42 @@ async function postUserSignup(req, res, next) {
 }
 
 async function postUserLogin(req, res, next) {
+    try {
+        await loginValidation(req.body);
 
+    } catch (err) {
+        return res.status(400).json({ error: err.details[0].message });
+    }
+
+    try {
+        // Data valid, check for an existing email
+        const { email, password } = req.body;
+        const { rows } = await db.query(SQL`
+            SELECT app_user.user_id, app_user.password
+            FROM app_user
+            WHERE app_user.email = ${email}
+        `);
+        const user = rows[0];
+
+        if (!user) {
+            return res.status(400).json({ error: "Email is not found." });
+        }
+
+        // Validate pw
+        const validPassword = await bcrypt.compare(password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({ error: "Invalid password." });
+        }
+
+        // Create and send token
+        const token = jwt.sign({ _id: user.user_id }, process.env.TOKEN_SECRET, { expiresIn: "2h" });
+
+        res.status(200).json({ accessToken: token });
+
+    } catch (err) {
+        next(err);
+    }
 }
 
 module.exports = {
