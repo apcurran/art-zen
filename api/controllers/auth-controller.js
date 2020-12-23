2087b9aa63851e960cb337a939d4773066cc017f
Alex Curran <alexcurran.va@gmail.com>
2020-12-23 14:57:13 -0600

    Added signupValidation to postUserSignup func in controller

--- api/controllers/auth-controller.js
+++ api/controllers/auth-controller.js
@@ -5,9 +5,18 @@ const SQL = require("sql-template-strings");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+const { signupValidation, loginValidation } = require("../validation/auth-validation");
+
 // POST controllers
 async function postUserSignup(req, res, next) {
+    try {
+        await signupValidation(req.body);
 
+    } catch (err) {
+        return res.status(400).json({ error: err.details[0].message });
+    }
+
+    res.send("signup route");
 }
 
 async function postUserLogin(req, res, next) {
