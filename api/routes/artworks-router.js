eca0aeb80851601c7972a81ce52da63b6e2f89d8
Alex Curran <alexcurran.va@gmail.com>
2020-12-29 10:13:47 -0600

    Removed UNIQUE constraints

--- api/routes/artworks-router.js
+++ api/routes/artworks-router.js
@@ -7,25 +7,25 @@ const artworksController = require("../controllers/artworks-controller");
 const verifyAuth = require("../middleware/verify-auth");
 
 // GET specific user artworks based on user id
-router.get("/users/:userId", artworksController.getUserArtworks);
+router.get("/users/:userId", artworksController.getUserArtworks); // tested
 // GET search results for artwork title or genre
-router.get("/search", artworksController.getSearch);
+router.get("/search", artworksController.getSearch); // tested
 // GET specific artwork based on artwork id
-router.get("/:artworkId", artworksController.getUserArtwork);
+router.get("/:artworkId", artworksController.getUserArtwork); // tested
 // GET all artworks
-router.get("/", artworksController.getArtworks);
+router.get("/", artworksController.getArtworks); // tested
 // POST new artwork like
 router.post("/:artworkId/likes", verifyAuth, artworksController.postUserArtworkLike); // tested
 // POST new artwork comment
 router.post("/:artworkId/comments", verifyAuth, artworksController.postUserArtworkComment); // tested
 // POST new artwork favorite
-router.post("/:artworkId/favorites", verifyAuth, artworksController.postUserArtworkFavorite);
+router.post("/:artworkId/favorites", verifyAuth, artworksController.postUserArtworkFavorite); // tested
 // POST new artwork
-router.post("/", verifyAuth, artworksController.postUserArtwork); 
+router.post("/", verifyAuth, artworksController.postUserArtwork); // 
 // DELETE artwork like
 router.delete("/:artworkId/likes/:likeId", verifyAuth, artworksController.deleteUserArtworkLike); // tested
 // DELETE artwork favorite
-router.delete("/:artworkId/favorites/:favoriteId", verifyAuth, artworksController.deleteUserArtworkFavorite);
+router.delete("/:artworkId/favorites/:favoriteId", verifyAuth, artworksController.deleteUserArtworkFavorite); // tested
 // DELETE artwork
 router.delete("/:artworkId", verifyAuth, artworksController.deleteUserArtwork);
 
--- db/db_schema.sql
+++ db/db_schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE app_user(
 
 CREATE TABLE artwork(
     artwork_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL UNIQUE,
+    user_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(500) NOT NULL,
@@ -19,15 +19,15 @@ CREATE TABLE artwork(
 );
 
 CREATE TABLE follower(
-    follower_user_id INT NOT NULL UNIQUE,
+    follower_user_id INT NOT NULL,
     FOREIGN KEY(follower_user_id) REFERENCES app_user(user_id) ON DELETE CASCADE,
-    account_user_id INT NOT NULL UNIQUE,
+    account_user_id INT NOT NULL,
     FOREIGN KEY(account_user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE artwork_like(
     like_id SERIAL PRIMARY KEY,
-    artwork_id INT NOT NULL UNIQUE,
+    artwork_id INT NOT NULL,
     FOREIGN KEY(artwork_id) REFERENCES artwork(artwork_id) ON DELETE CASCADE,
     user_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
@@ -35,9 +35,9 @@ CREATE TABLE artwork_like(
 
 CREATE TABLE artwork_comment(
     comment_id SERIAL PRIMARY KEY,
-    artwork_id INT NOT NULL UNIQUE,
+    artwork_id INT NOT NULL,
     FOREIGN KEY(artwork_id) REFERENCES artwork(artwork_id) ON DELETE CASCADE,
-    user_id INT NOT NULL UNIQUE,
+    user_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE,
     text VARCHAR(500) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -45,8 +45,8 @@ CREATE TABLE artwork_comment(
 
 CREATE TABLE artwork_favorite(
     favorite_id SERIAL PRIMARY KEY,
-    artwork_id INT NOT NULL UNIQUE,
+    artwork_id INT NOT NULL,
     FOREIGN KEY(artwork_id) REFERENCES artwork(artwork_id) ON DELETE CASCADE,
-    user_id INT NOT NULL UNIQUE,
+    user_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES app_user(user_id) ON DELETE CASCADE
 );
