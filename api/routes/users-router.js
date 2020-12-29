40f25c2f4602335d6bd1af6a299eb44caff43ea5
Alex Curran <alexcurran.va@gmail.com>
2020-12-29 11:30:51 -0600

    Routed tested

--- api/routes/users-router.js
+++ api/routes/users-router.js
@@ -7,12 +7,12 @@ const usersController = require("../controllers/users-controller");
 const verifyAuth = require("../middleware/verify-auth");
 
 // POST new account follower
-router.post("/:userId/followers", verifyAuth, usersController.postUserFollower);
+router.post("/:userId/followers", verifyAuth, usersController.postUserFollower); // tested
 // PATCH user account
 router.patch("/:userId", verifyAuth, usersController.patchUser); // tested
 // DELETE account follower
-router.delete("/:userId/followers/:followerId", verifyAuth, usersController.deleteUserFollower);
+router.delete("/:userId/followers/:followerId", verifyAuth, usersController.deleteUserFollower); // tested
 // DELETE user account
-router.delete("/:userId", verifyAuth, usersController.deleteUser);
+router.delete("/:userId", verifyAuth, usersController.deleteUser); // tested
 
 module.exports = router;
