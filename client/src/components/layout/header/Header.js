839066dbc951d3bc0f4f04dc7340e526d7dad102
Alex Curran <alexcurran.va@gmail.com>
2020-12-30 13:04:14 -0600

    Added SearchBar comp and styling

--- client/src/components/layout/header/Header.js
+++ client/src/components/layout/header/Header.js
@@ -2,6 +2,7 @@ import { NavLink } from "react-router-dom";
 
 import "./Header.css";
 import Logo from "../../../assets/images/logo-finished-opt.svg";
+import SearchBar from "./SearchBar";
 import SignedOutLinks from "./SignedOutLinks";
 
 function Header() {
@@ -11,6 +12,7 @@ function Header() {
                 <NavLink to="/" className="nav__logo-link">
                     <img className="nav__logo-img" src={Logo} alt="Art Zen logo"/>
                 </NavLink>
+                <SearchBar />
                 <SignedOutLinks />
             </nav>
         </header>
---
+++ client/src/components/layout/header/SearchBar.js
@@ -0,0 +1,9 @@
+function SearchBar() {
+    return (
+        <form className="search-form">
+            <input type="text" className="search-form__input" placeholder="Search for artworks..."/>
+        </form>
+    );
+}
+
+export default SearchBar;
