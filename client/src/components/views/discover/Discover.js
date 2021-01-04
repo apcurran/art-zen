5e6697cae63e5b5f46563ff5e7c2c3a5b6f98b6b
Alex Curran <alexcurran.va@gmail.com>
2021-01-04 16:11:11 -0600

    Deleted package-lock.json and node_modules in client dir, then re-installed dependencies. This resolved fetch err in React.

--- client/src/components/views/discover/Discover.js
+++ client/src/components/views/discover/Discover.js
@@ -3,6 +3,12 @@ import { useState, useEffect } from "react";
 import "./Discover.css";
 
 function Discover() {
+    useEffect(() => {
+        fetch("/api/artworks")
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }, []);
+
     return (
         <div>
             Discover new artists
