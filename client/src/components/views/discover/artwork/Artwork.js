868b19c6a75822ff1e0dfc5a21e9075b93e66330
Alex Curran <alexcurran.va@gmail.com>
2021-01-04 16:45:30 -0600

    Cloudinary images coming in correctly with Image component

--- client/src/components/views/discover/artwork/Artwork.js
+++ client/src/components/views/discover/artwork/Artwork.js
@@ -1,6 +1,8 @@
+import { Image } from "cloudinary-react";
+
 function Artwork({ img_url }) {
     return (
-        <img src={img_url} alt="artwork image"/>
+        <Image cloudName="dev-project" publicId={img_url} width="300" crop="scale" />
     );
 }
 
