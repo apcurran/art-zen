ab5feb020d1f3fbf930fb047953166115c2cf2e8
Alex Curran <alexcurran.va@gmail.com>
2021-01-05 10:32:30 -0600

    Data fetching correctly

--- client/src/components/views/artwork-view/ArtworkView.js
+++ client/src/components/views/artwork-view/ArtworkView.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 
 function ArtworkView() {
     const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`/api/artworks/${id}`)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err));
+    }, []);
 
     return (
         <div>
