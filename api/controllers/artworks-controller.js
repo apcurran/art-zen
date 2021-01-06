81287e1180528399836fde3924ae152f885ae04a
Alex Curran <alexcurran.va@gmail.com>
2021-01-06 13:43:39 -0600

    Updated SQL query for user artworks profile to CAST str vals as ints

--- api/controllers/artworks-controller.js
+++ api/controllers/artworks-controller.js
@@ -104,12 +104,12 @@ async function getUserArtworks(req, res, next) {
                 artwork.artwork_id, artwork.user_id, artwork.img_url AS artwork_img_url,
                 app_user.username, app_user.avatar_img_url, app_user.bio_description,
                 (
-                    SELECT COUNT(artwork.artwork_id)
+                    SELECT CAST(COUNT(artwork.artwork_id) AS int)
                     FROM artwork
                     WHERE artwork.user_id = ${userId}
                 ) AS total_creations,
                 (
-                    SELECT COUNT(follower.follower_user_id)
+                    SELECT CAST(COUNT(follower.follower_user_id) AS int)
                     FROM follower
                     WHERE follower.account_user_id = ${userId}
                 ) AS total_followers
--- client/src/components/views/user-profile/UserProfile.js
+++ client/src/components/views/user-profile/UserProfile.js
@@ -1,9 +1,19 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import "./UserProfile.css";
 
 function UserProfile() {
     const { id } = useParams();
+
+    useEffect(() => {
+        fetch(`/api/artworks/users/${id}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => console.error(err));
+    }, [id]);
 
     return (
         <div>
