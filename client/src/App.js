4aef4c3d358b895d7f38f4b5cfa4003b5ff7b742
Alex Curran <alexcurran.va@gmail.com>
2021-01-07 16:19:11 -0600

    Lifted context up to index.js, to check login status in App component on startup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
+import { useContext, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import AuthContextProvider from "./contexts/AuthContext";
+import { AuthContext } from "./contexts/AuthContext";
 
 import Header from "./components/layout/header/Header";
 import About from "./components/views/about/About";
@@ -10,22 +11,25 @@ import UserProfile from "./components/views/user-profile/UserProfile";
 import Footer from "./components/layout/footer/Footer";
 
 function App() {
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
+
+  useEffect(() => {
+    // Run auth check on app startup
+    localStorage.authToken ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  }, []);
+
   return (
     <Router>
-      <AuthContextProvider>
-
-        <div className="App">
-          <Header />
-          <Switch>
-            <Route path="/artworks/users/:id" component={UserProfile} />
-            <Route path="/artworks/:id" component={ArtworkView} />
-            <Route path="/about" component={About} />
-            <Route path="/" component={Discover} />
-          </Switch>
-          <Footer />
-        </div>
-
-      </AuthContextProvider>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/artworks/users/:id" component={UserProfile} />
+          <Route path="/artworks/:id" component={ArtworkView} />
+          <Route path="/about" component={About} />
+          <Route path="/" component={Discover} />
+        </Switch>
+        <Footer />
+      </div>
     </Router>
   );
 }
