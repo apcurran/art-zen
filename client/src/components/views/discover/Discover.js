a2ad9440884ef3ff4d4225b408bca060fc9645cb
Alex Curran <alexcurran.va@gmail.com>
2021-01-19 11:24:59 -0600

    title highlight effect in place

--- client/src/components/views/discover/Discover.js
+++ client/src/components/views/discover/Discover.js
@@ -9,7 +9,9 @@ function Discover() {
 
     return (
         <div className="discover">
-            <h1 className="discover__title">Discover New Artists</h1>
+            <div className="title--highlight-wrapper">
+                <h1 className="discover__title title--highlight">Discover New Artists</h1>
+            </div>
             <main className="masonry-grid">
                 {artworks.map(artwork => (
                     <GridArtwork img_url={artwork.img_url} artwork_id={artwork.artwork_id} key={artwork.artwork_id} />
