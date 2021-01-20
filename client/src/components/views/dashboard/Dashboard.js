469e0a1b8a8d9d6fe95cc83aff091dd6e5f67f0c
Alex Curran <alexcurran.va@gmail.com>
2021-01-20 10:03:59 -0600

    Added route guard for dashboard

--- client/src/components/views/dashboard/Dashboard.js
+++ client/src/components/views/dashboard/Dashboard.js
@@ -1,4 +1,7 @@
-import { Route, Switch } from "react-router-dom";
+import { useContext } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import { AuthContext } from "../../../contexts/AuthContext";
 
 import "./Dashboard.css";
 import DashboardNav from "./dashboard-nav/DashboardNav";
@@ -6,6 +9,11 @@ import UserInfo from "./user-info/UserInfo";
 import MyArtwork from "./my-artwork/MyArtwork";
 
 function Dashboard() {
+    const { isLoggedIn } = useContext(AuthContext);
+
+    // Route guard
+    if (!isLoggedIn) return <Redirect to="/auth/log-in" />
+
     return (
         <main className="dashboard-main">
             <h1>Dashboard</h1>
