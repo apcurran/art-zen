12e9deb0b6c21aee3f9f42eff070978935e6705a
Alex Curran <alexcurran.va@gmail.com>
2021-02-02 16:27:03 -0600

    Lazy loaded imgs below-the-fold for About and ArtworkView pages

--- client/src/components/views/about/About.js
+++ client/src/components/views/about/About.js
@@ -30,13 +30,13 @@ function About() {
                     </p>
                 </div>
                 <figure className="main-about__section__fig">
-                    <img src={SharingArticlesIllustration} alt="Man and a woman text artwork to each other." width="989" height="766" className="main-about__section__fig__img"/>
+                    <img src={SharingArticlesIllustration} alt="Man and a woman text artwork to each other." width="989" height="766" loading="lazy" className="main-about__section__fig__img"/>
                 </figure>
             </section>
             <svg className="wave-svg wave-svg--bottom" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 100" preserveAspectRatio="none"><path className="wavePath-haxJK1" d="M826.337463,25.5396311 C670.970254,58.655965 603.696181,68.7870267 447.802481,35.1443383 C293.342778,1.81111414 137.33377,1.81111414 0,1.81111414 L0,150 L1920,150 L1920,1.81111414 C1739.53523,-16.6853983 1679.86404,73.1607868 1389.7826,37.4859505 C1099.70117,1.81111414 981.704672,-7.57670281 826.337463,25.5396311 Z" fill="currentColor"></path></svg>
             <section className="main-about__section">
                 <figure className="main-about__section__fig">
-                    <img src={MonsterArtistIllustration} alt="Monster with a mustache displays artwork with orange circles." width="738" height="401" className="main-about__section__fig__img"/>
+                    <img src={MonsterArtistIllustration} alt="Monster with a mustache displays artwork with orange circles." width="738" height="401" loading="lazy" className="main-about__section__fig__img"/>
                 </figure>
                 <div className="main-about__section__content">
                     <h2 className="main-about__section__title">
