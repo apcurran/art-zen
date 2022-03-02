00e2f346221cb900068855bca50157af6605f814
Alex Curran <alexcurran.va@gmail.com>
2022-03-02 12:17:09 -0600

    Test artworks home page card navigation to specific artwork page

--- cypress/integration/artworks-home/artworks.spec.js
+++ cypress/integration/artworks-home/artworks.spec.js
@@ -1,11 +1,25 @@
 /// <reference types="cypress" />
 
 describe("artworks home page", () => {
-    it("displays artorks when visited", () => {
+    beforeEach(() => {
         cy.visit("/");
+    });
 
+    it("displays artorks when visited", () => {
         cy.get(".masonry-grid")
             .children()
             .should("have.length.greaterThan", 0);
+    });
+
+    it("clicking a card navigates to the unique artwork page", () => {
+        cy.get("a[href=\"/artworks/81\"]")
+            .should("exist")
+            .click();
+        
+        cy.url()
+            .should("eq", "http://localhost:3000/artworks/81");
+
+        cy.contains("h1", "Cartoon Life")
+            .should("exist");
     });
 });
