ca666bb8922a6f074e015fc29d29d00f962978f4
Alex Curran <alexcurran.va@gmail.com>
2022-03-03 14:11:58 -0600

    Add cypress command for cy.login() functionality utilizing cy.request() to programmatically log in without using the UI to build up state each time log in functionality is needed

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -10,9 +12,23 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
+Cypress.Commands.add("login", () => {
+    cy.request({
+        method: "POST",
+        url: "http://localhost:3000/api/auth/log-in",
+        body: {
+            email: Cypress.env("testUserEmail"),
+            password: Cypress.env("testUserPassword")
+        }
+    })
+    .then((response) => {
+        const { accessToken, userId } = response.body;
+
+        window.localStorage.setItem("authToken", accessToken);
+        window.localStorage.setItem("userId", userId);
+    });
+});
+
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
 //
