628db90de90ebc3464efd20d1fd93d4b1b194ebb
Alex Curran <alexcurran.va@gmail.com>
2022-03-03 11:20:30 -0600

    Rename dir and add test for 'all user artworks page'

---
+++ cypress/integration/user-artwork/all-user-artworks.spec.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe("all user artworks page", () => {
+    it("can navigate to another user's artworks page, and the page displays all relevant user info and available artwork cards", () => {
+        cy.visit("/artworks/28");
+
+        cy.contains("a", "Rosie.Graham1")
+            .should("exist")
+            .click();
+
+        // now on all user artworks page
+        cy.get(".user-profile__info__avatar__img")
+            .should("exist");
+
+        cy.contains("h2", "Rosie.Graham1")
+            .should("exist");
+
+        cy.get(".user-profile__info-sect__totals")
+            .children()
+            .first()
+            .should("not.be.empty");
+
+        cy.get(".user-profile__info-sect__totals")
+            .children()
+            .last()
+            .should("not.be.empty");
+
+        cy.get(".user-profile__info__bio")
+            .should("not.be.empty");
+
+        cy.get(".user-profile__artworks-grid")
+            .children()
+            .should("have.length.above", 0);
+    });
+});
