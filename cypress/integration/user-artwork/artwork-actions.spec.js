90d52bb85148e42be0222eeea04fe5340351a7e0
Alex Curran <alexcurran.va@gmail.com>
2022-03-08 11:51:17 -0600

    Test for adding a user comment to an artwork page

--- cypress/integration/user-artwork/artwork-actions.spec.js
+++ cypress/integration/user-artwork/artwork-actions.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("specific artwork page user actions", () => {
-    beforeEach(() => {
+    before(() => {
         cy.login();
 
         cy.visit("/artworks/81");
@@ -13,8 +13,8 @@ describe("specific artwork page user actions", () => {
             statusCode: 201,
             body: {
                 "likesData": {
-                    "like_id":93,
-                    "user_id":36
+                    "like_id": 93,
+                    "user_id": 36
                 }
             }
         });
@@ -51,5 +51,49 @@ describe("specific artwork page user actions", () => {
         cy.get(".artwork-view__info__social-data__totals")
             .last()
             .should("have.text", "1 Favorites");
+    });
+
+    it("comment will be posted below artwork after submitting", () => {
+        cy.intercept("POST", "/api/artworks/81/comments", {
+            statusCode: 201,
+            body: {
+                commentsData: [
+                    {
+                        "comment_id": 61,
+                        "user_id": 36,
+                        "text": "Test here",
+                        "comment_created_at": "2022-03-08T17:26:33.118Z",
+                        "comment_username": "John",
+                        "comment_avatar_img": "https://res.cloudinary.com/dev-project/image/upload/v1644616038/art-zen-app/user-avatars/alugkzxsgetx1hcjxfmi.png"
+                    }
+                ]
+            }
+        });
+
+        cy.get("textarea")
+            .type("Hello world!");
+
+        cy.contains("button", /submit/i)
+            .click();
+
+        cy.get(".comment-segment")
+            .should("exist")
+            .contains("h4", "John")
+            .should("exist");
+            
+        cy.get(".comment-segment")
+            .contains("time", "Mar 8th, 2022")
+            .should("exist");
+
+        cy.get(".comment-segment")
+            .contains("p", "Test here")
+            .should("exist");
+
+        cy.get(".comment-segment__info__delete-btn")
+            .contains("span", /delete/i)
+            .should("exist");
+
+        cy.get(".artwork-view__comments-total__amt")
+            .should("have.text", "1");
     });
 });
