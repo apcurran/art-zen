00730a4d219b0fd38ccf2215bb4cd94811e31380
Alex Curran <alexcurran.va@gmail.com>
2022-03-09 14:14:58 -0600

    Update db config to include a local db for testing purposes

--- db/index.js
+++ db/index.js
@@ -4,12 +4,21 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const pool = new Pool({
+const devConfig = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+};
+const prodConfig = {
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
-});
+}; 
+
+const pool = process.env.NODE_ENV === "development" ? new Pool(devConfig) : new Pool(prodConfig);
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
