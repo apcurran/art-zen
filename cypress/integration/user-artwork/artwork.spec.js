9528e2430d44253b9547ac6dca879bf0f6a5a760
Alex Curran <alexcurran.va@gmail.com>
2022-03-14 13:32:52 -0500

    Update tests to work with local testing db

--- cypress/integration/user-artwork/artwork.spec.js
+++ cypress/integration/user-artwork/artwork.spec.js
@@ -2,26 +2,26 @@
 
 describe("specific artwork page", () => {
     beforeEach(() => {
-        cy.visit("/artworks/81");
+        cy.visit("/artworks/1");
     });
 
     it("displays all relevant artwork info", () => {
         cy.get("img")
             .should("have.class", "artwork-view__info__fig__img");
 
-        cy.contains("h1", "Cartoon Life")
+        cy.contains("h1", "Customizable methodical task-force")
             .should("exist");
         
-        cy.contains("a", "tester")
+        cy.contains("a", "Amir.Champlin39")
             .should("exist");
 
-        cy.contains("span", /fantasy/i)
+        cy.contains("span", /modern/i)
             .should("exist");
 
         cy.get(".artwork-view__info__desc")
             .should("not.be.empty");
 
-        cy.contains("time", "Nov 29th, 2021")
+        cy.contains("time", "Mar 9th, 2022")
             .should("exist");
     });
 });
