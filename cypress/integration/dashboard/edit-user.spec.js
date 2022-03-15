e4cbd04f5437241d3bd7e6ffccdc48ddc2984515
Alex Curran <alexcurran.va@gmail.com>
2022-03-15 13:26:18 -0500

    Test for user bio description edit functionality

--- cypress/integration/dashboard/edit-user.spec.js
+++ cypress/integration/dashboard/edit-user.spec.js
@@ -4,16 +4,6 @@ describe("edit user profile", () => {
     const userId = 33;
 
     beforeEach(() => {
-        cy.login();
-
-        cy.visit("/dashboard/user-info");
-    });
-
-    it("user should be able to change bio description of profile", () => {
-        
-    });
-
-    it("user should be able to change avatar image of profile", () => {
         cy.intercept("PATCH", `/api/users/${userId}`, {
             statusCode: 200,
             body: {
@@ -21,6 +11,24 @@ describe("edit user profile", () => {
             }
         });
 
+        cy.login();
+
+        cy.visit("/dashboard/user-info");
+    });
+
+    it("user should be able to change bio description of profile", () => {
+        cy.get(".dashboard-user-info__textarea")
+            .clear()
+            .type("lorem ipsum");
+
+        cy.contains("button", /update/i)
+            .click();
+
+        cy.contains("User info updated")
+            .should("exist");
+    });
+
+    it("user should be able to change avatar image of profile", () => {
         cy.fixture("images/doom-suspect.png")
             .as("doomGuy");
 
