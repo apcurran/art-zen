75509a8209272ad0287c465b0bcbe604a341ac4a
Alex Curran <alexcurran.va@gmail.com>
2022-03-15 15:20:54 -0500

    Add img fixture and test for adding a new user artwork from the dashboard comp

---
+++ cypress/integration/dashboard/add-artwork.spec.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+describe("add artwork -- dashboard", () => {
+    beforeEach(() => {
+        cy.login();
+
+        cy.visit("/dashboard/add-artwork");
+    });
+
+    it("user can fill out form details and submit the form", () => {
+        cy.intercept("POST", "/api/artworks", {
+            statusCode: 201,
+            body: {
+                addedArtwork: {
+                    artwork_id: 34,
+                    img_url: "art-zen-app/urrkbqdtcvle4x6fqxun"
+                }
+            }
+        });
+
+        cy.get("input[id=title]")
+            .type("Cartoon Aliens");
+
+        cy.get("select[id=genre]")
+            .select("sci-fi");
+
+        cy.get("textarea[id=description]")
+            .type("Here is my example description.");
+
+        cy.fixture("images/cartoon-aliens.jpg")
+            .as("cartoonAliensImg");
+        
+        cy.get("input[type=file]")
+            .selectFile("@cartoonAliensImg");
+
+        cy.get("input[id=artwork-img-alt-txt]")
+            .type("Black and white cartoon aliens in a spaceship.");
+
+        cy.contains("button", /upload/i)
+            .click();
+
+        // result
+        cy.contains("Your new artwork was successfully uploaded!")
+            .should("exist");
+    });
+});
