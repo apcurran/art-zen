6c13e8a8118d23620531f81d22f03987db2a26df
Alex Curran <alexcurran.va@gmail.com>
2022-03-17 14:47:01 -0500

    Add wrapping figure elem

--- client/src/components/views/discover/grid-artwork/GridArtwork.js
+++ client/src/components/views/discover/grid-artwork/GridArtwork.js
@@ -6,15 +6,17 @@ import "./GridArtwork.css";
 function GridArtwork({ img_url, img_alt_txt, artwork_id, title, username }) {
     return (
         <Link to={{pathname: `/artworks/${artwork_id}`}} className="masonry-grid__link">
-            <Image
-                className="masonry-grid__link__img"
-                cloudName="dev-project"
-                publicId={img_url}
-                alt={`${title} by ${username} -- ${img_alt_txt}`}
-                decoding="async"
-            >
-                <Transformation quality="auto" fetchFormat="auto" height="375" width="auto" crop="scale" />
-            </Image>
+            <figure className="masonry-grid__link__fig">
+                <Image
+                    className="masonry-grid__link__img"
+                    cloudName="dev-project"
+                    publicId={img_url}
+                    alt={`${title} by ${username} -- ${img_alt_txt}`}
+                    decoding="async"
+                >
+                    <Transformation quality="auto" fetchFormat="auto" height="375" width="auto" crop="scale" />
+                </Image>
+            </figure>
         </Link>
     );
 }
