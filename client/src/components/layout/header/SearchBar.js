ff5c32f169adddde04690af61a8fc1f646d738d0
Alex Curran <alexcurran.va@gmail.com>
2022-03-21 14:26:57 -0500

    Revise comp for better searching by utilizing a debounce for sending API req as search bar text changes (only after 300ms of stopping)

--- client/src/components/layout/header/SearchBar.js
+++ client/src/components/layout/header/SearchBar.js
@@ -1,31 +1,40 @@
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
 
 import { DiscoverArtworksContext } from "../../../contexts/DiscoverArtworksContext";
 
 function SearchBar() {
     const { setArtworks } = useContext(DiscoverArtworksContext);
     const [searchText, setSearchText] = useState("");
-    const navigate = useNavigate();
 
-    async function handleSearchSubmit(event) {
-        event.preventDefault();
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            handleSearchFetch();
+        }, 300); // timeout of 300ms
 
+        return () => clearTimeout(timeout);
+    }, [searchText]);
+
+    async function handleInputChange(event) {
+        const revisedSearchText = event.target.value;
+        setSearchText(revisedSearchText);
+    }
+    
+    async function handleSearchFetch() {
         try {
             const response = await fetch(`/api/artworks/search?q=${searchText}`);
             const searchResultsData = await response.json();
-
+    
             setArtworks(searchResultsData);
-            navigate("/");
-
+    
         } catch (err) {
             console.error(err);
         }
     }
 
+
     return (
-        <form onSubmit={handleSearchSubmit} className="search-form">
-            <input value={searchText} onChange={(event) => setSearchText(event.target.value)} type="text" className="search-form__input" aria-label="Search for artworks" placeholder="Search for artworks by title or genre"/>
+        <form className="search-form">
+            <input value={searchText} onChange={handleInputChange} type="text" className="search-form__input" aria-label="Search for artworks" placeholder="Search for artworks by title or genre"/>
         </form>
     );
 }
