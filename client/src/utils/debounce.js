cc0aca13f6eaccaf1289e69a5674f49c3ae1b463
Alex Curran <alexcurran.va@gmail.com>
2022-03-21 13:50:56 -0500

    Add debounce util func

---
+++ client/src/utils/debounce.js
@@ -0,0 +1,10 @@
+function debounce(func, ms) {
+    let timeout;
+
+    return function() {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, arguments), ms);
+    };
+}
+
+export { debounce };
