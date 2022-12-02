20c2c115e51ea151c33710e7dee34940f3feba90
Alex Curran <alexcurran.va@gmail.com>
2022-12-02 14:31:01 -0600

    Revert back rejectUnauthorized setting since server cannot have a valid self-signed SSL certificate

--- db/index.js
+++ db/index.js
@@ -14,7 +14,7 @@ const devConfig = {
 const prodConfig = {
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: true
+        rejectUnauthorized: false
     }
 };
 const connection = process.env.NODE_ENV === "development" ? devConfig : prodConfig;
