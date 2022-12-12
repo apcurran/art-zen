84c663c76e61507305d046157f839c8b65d277fa
Alex Curran <alexcurran.va@gmail.com>
2022-12-12 16:00:43 -0600

    Add error handling for expired tokens

--- api/middleware/verify-auth.js
+++ api/middleware/verify-auth.js
@@ -3,21 +3,26 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-   const authHeader = req.header("Authorization");
-   const token = authHeader && authHeader.split(" ")[1];
-   
-   if (!token) {
-       return res.status(401).json({ error: "Access denied." });
-   }
+    const authHeader = req.header("Authorization");
+    const token = authHeader && authHeader.split(" ")[1];
 
-   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-       if (err) {
-           console.error(err);
-           return res.status(403).json({ error: "Invalid token." });
-       }
+    if (!token) {
+        return res.status(401).json({ error: "Access denied." });
+    }
 
-       // Validation passed
-       req.user = user;
-       next();
-   });
+    jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+        if (err && err.name === "TokenExpiredError") {
+            // handle expired token
+            return res.status(403).json({ error: "Expired token" });
+        }
+
+        if (err) {
+            console.error(err);
+            return res.status(403).json({ error: "Invalid token" });
+        }
+
+        // Validation passed
+        req.user = user;
+        next();
+    });
 };
