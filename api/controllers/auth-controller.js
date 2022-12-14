b30b524763a18a678142b9c7c0aec6bc37c8b0f0
Alex Curran <alexcurran.va@gmail.com>
2022-12-14 13:42:48 -0600

    Revert expiresIn token time

--- api/controllers/auth-controller.js
+++ api/controllers/auth-controller.js
@@ -69,7 +69,7 @@ async function postUserLogin(req, res, next) {
         }
 
         // Create and send token
-        const token = jwt.sign({ _id: user.user_id }, process.env.TOKEN_SECRET, { expiresIn: "15s" });
+        const token = jwt.sign({ _id: user.user_id }, process.env.TOKEN_SECRET, { expiresIn: "2h" });
 
         res.status(200).json({ accessToken: token, userId: user.user_id });
 
