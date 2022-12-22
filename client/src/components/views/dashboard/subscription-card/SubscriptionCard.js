7415a4f07814f2bbe75fb03f0b3062ad620a6722
Alex Curran <alexcurran.va@gmail.com>
2022-12-22 15:06:41 -0600

    Remove nesting of <a> elements

--- client/src/components/views/dashboard/subscription-card/SubscriptionCard.js
+++ client/src/components/views/dashboard/subscription-card/SubscriptionCard.js
@@ -1,5 +1,5 @@
-import { Image, Transformation } from "cloudinary-react";
 import { Link } from "react-router-dom";
+import { Image, Transformation } from "cloudinary-react";
 
 import "./SubscriptionCard.css";
 
@@ -8,8 +8,8 @@ import formatDate from "../../../../utils/format-date";
 
 function SubscriptionCard({ artwork }) {
     return (
-        <Link to={{pathname: `/artworks/${artwork.artwork_id}`}} className="subscriptions-grid__card-link-container">
-            <article className="subscriptions-grid__card">
+        <article className="subscriptions-grid__card">
+            <Link to={{ pathname: `/artworks/${artwork.artwork_id}` }} className="subscriptions-grid__card-link-container">
                 <figure className="subscriptions-grid__card__fig">
                     <Image
                         className="subscriptions-grid__card__fig__img"
@@ -25,11 +25,11 @@ function SubscriptionCard({ artwork }) {
                 </figure>
                 <h3 className="subscriptions-grid__card__title">{artwork.title}</h3>
                 <Chip passedClass="subscriptions-grid__card__genre-chip">{artwork.genre}</Chip>
-                <p className="subscriptions-grid__card__user-desc">
-                    Posted by <Link to={{pathname: `/artworks/users/${artwork.user_id}`}} className="subscriptions-grid__card__user-desc__user-link">{artwork.username}</Link> on <time>{formatDate(artwork.created_at)}</time>
-                </p>
-            </article>
-        </Link>
+            </Link>
+            <p className="subscriptions-grid__card__user-desc">
+                Posted by <Link to={{ pathname: `/artworks/users/${artwork.user_id}` }} className="subscriptions-grid__card__user-desc__user-link">{artwork.username}</Link> on <time>{formatDate(artwork.created_at)}</time>
+            </p>
+        </article>
     );
 }
 
