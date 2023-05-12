21a4e67088d7d7c550437b461de39cc962f080ab
Alex Curran <alexcurran.va@gmail.com>
2023-05-12 12:38:12 -0500

    Add db connection and disconnect to script

--- db/avatar-img-update-script.js
+++ db/avatar-img-update-script.js
@@ -6,13 +6,21 @@ const { db } = require("../db/index");
 
 async function updateAvatarImages() {
     try {
-        const images = await db.manyOrNone(`
+        console.log("Creating a new db connection");
+        const dbConnection = await db.connect();
+        const images = await dbConnection.manyOrNone(`
             SELECT avatar_img_url
             FROM app_user
         `);
 
-        console.log(images);
-        
+        // iterate list
+        for (let i = 0; i < images.length; i++) {
+            // generate avatar img
+
+            // update avatar img with new one
+        }
+        console.log("Disconnecting temp db connection");
+        await dbConnection.done();
 
     } catch (err) {
         console.error(err);
