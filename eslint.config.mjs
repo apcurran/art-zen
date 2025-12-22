0566332a2c63858e026172ca24e730ec842d99db
Alex Curran <alexcurran.va@gmail.com>
2025-12-22 14:18:18 -0600

    Eslint upgraded to v9 along with `eslint-plugin-cypress` plugin

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import pluginCypress from "eslint-plugin-cypress";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    },
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    // react scoped to /client dir
+    {
+        files: ["client/**/*.{js,jsx}"],
+        ...pluginReact.configs.flat.recommended,
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+    // cypress plugin setup
+    {
+        files: ["cypress/**/*.js"],
+        extends: [pluginCypress.configs.recommended],
+    },
+    {
+        files: ["**/*.json"],
+        plugins: { json },
+        language: "json/json",
+        extends: ["json/recommended"],
+    },
+    {
+        files: ["**/*.md"],
+        plugins: { markdown },
+        language: "markdown/gfm",
+        extends: ["markdown/recommended"],
+    },
+]);
