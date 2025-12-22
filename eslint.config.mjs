3a91b48ba11dd68c78d72b422e6c4653658bac20
Alex Curran <alexcurran.va@gmail.com>
2025-12-22 14:38:23 -0600

    Silence react var needed in scope error for react client files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,6 +31,9 @@ export default defineConfig([
                 version: "detect",
             },
         },
+        rules: {
+            "react/react-in-jsx-scope": "off",
+        },
     },
     // cypress plugin setup
     {
