721d5d53e3d95aae31b6adbbc7080139f88b1f5a
Alex Curran <alexcurran.va@gmail.com>
2025-12-30 11:54:58 -0600

    Make test less brittle, remove reliance on specific user id

--- cypress/e2e/user-artwork/all-user-artworks.cy.js
+++ cypress/e2e/user-artwork/all-user-artworks.cy.js
@@ -2,14 +2,23 @@
 
 describe("all user artworks page", () => {
     it("can navigate to another user's artworks page, and the page displays all relevant user info and available artwork cards", () => {
-        cy.visit("/artworks/28");
+        cy.visit("/");
 
-        cy.contains("a", "Jan78").should("exist").click();
+        // click first card
+        cy.get("a.masonry-grid__link").first().click();
+
+        // go to all artworks by clicking on artist name link
+        cy.get("a.artwork-view__info__author").should("exist").click();
 
         // now on all user artworks page
-        cy.get(".user-profile__info__avatar__img").should("exist");
+        cy.get(".user-profile__info__avatar__img")
+            .should("exist")
+            .and("be.visible");
 
-        cy.contains("h2", "Jan78").should("exist");
+        cy.get(".user-profile__info-sect")
+            .find("h2")
+            .should("exist")
+            .and("not.be.empty");
 
         cy.get(".user-profile__info-sect__totals")
             .children()
