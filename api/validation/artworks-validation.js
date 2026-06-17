e5c876ecae8eeb20c992f811ae9287652af07fbf
Alex Curran <alexcurran.va@gmail.com>
2026-06-17 13:46:14 -0500

    Add import package syntax

--- api/validation/artworks-validation.js
+++ api/validation/artworks-validation.js
@@ -1,4 +1,4 @@
-const Joi = require("joi");
+import Joi from "joi";
 
 export function userArtworkValidation(data) {
     const schema = Joi.object({
--- api/validation/auth-validation.js
+++ api/validation/auth-validation.js
@@ -1,4 +1,4 @@
-const Joi = require("joi");
+import Joi from "joi";
 
 export function signupValidation(data) {
     const schema = Joi.object({
