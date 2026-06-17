99e5dc2187dfb0cdedef272f5bc758c791a2b851
Alex Curran <alexcurran.va@gmail.com>
2026-06-17 12:20:42 -0500

    Update ESM

--- db/index.js
+++ db/index.js
@@ -1,22 +1,23 @@
-"use strict";
+import pgPromise from "pg-promise";
 
-const pgp = require("pg-promise")({ capSQL: true });
+const pgp = pgPromise({ capSQL: true });
 
 const devConfig = {
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
 };
+
 const prodConfig = {
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
 };
+
 const connection =
     process.env.NODE_ENV === "development" ? devConfig : prodConfig;
-const db = pgp(connection);
 
-module.exports = { db };
+export const db = pgp(connection);
